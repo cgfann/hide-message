4fbe02a798d7311a7eadf4dd4c40ed3835d71bb5
Charlotte Fanning <fanncg18@wfu.edu>
2021-04-03 19:30:59 -0400

    i think it worked

--- lab6.c
+++ lab6.c
@@ -18,24 +18,25 @@
 
 
 int process_header(FILE *wav_file, FILE *new_wav_file, short *sample_size_ptr, int *num_samples_ptr);
-void process_data(FILE* wave_file, FILE *new_wav_file, FILE *text_file, short sample_size, int num_samples, int bits);
+int process_data(FILE* wave_file, FILE *new_wav_file, FILE *text_file, short sample_size, int num_samples, int num_lsb);
 
 
 int main(int argc, char *argv[]) 
 {
-    FILE *wav_file;     /* .wav file read */
-    FILE *new_wav_file; /* to hide message */
-    FILE *text_file;    /* message */
-    short sample_size;  /* size of an audio sample (bytes) */
-    int num_samples;    /* number of audio samples, includes all channels */  
-    int bits;           /* number of least significant bits contributing to the message per sample*/
+    FILE *wav_file;             /* .wav file read */
+    FILE *new_wav_file;         /* to hide message */
+    FILE *text_file;            /* message */
+    short sample_size;          /* size of an audio sample (bytes) */
+    int num_samples;            /* number of audio samples, includes all channels */  
+    int num_lsb;                /* number of least significant bits contributing to the message per sample */
+    int num_chars = 0;          /* number of characters in the message */
     char wav_file_name[256];
     char text_file_name[256];
     char new_wav_file_name[256];
     int args_ok = 0;
     int header_ok = 0;    
 
-    args_ok = get_wav_args(argc, argv, &bits, wav_file_name, text_file_name);
+    args_ok = get_wav_args(argc, argv, &num_lsb, wav_file_name, text_file_name);
     if (!args_ok) {
         return 1;
     }
@@ -72,7 +73,9 @@ int main(int argc, char *argv[])
         printf("could not open .txt file %s \n", text_file_name);
         return 3;
     }
-    process_data(wav_file, new_wav_file, text_file, sample_size, num_samples, bits);
+
+    num_chars = process_data(wav_file, new_wav_file, text_file, sample_size, num_samples, num_lsb);
+    printf("%d characters written to %s \n", num_chars, new_wav_file_name);
 
     if (wav_file) {
         fclose(wav_file);
@@ -88,19 +91,17 @@ int main(int argc, char *argv[])
 
 
 /**
- *  function reads the RIFF, fmt, and start of the data chunk to collect information for reading the data
+ *  function reads the RIFF, fmt, and start of the data chunk to collect information for reading/copying the data
  **/
 int process_header(FILE *wav_file, FILE *new_wav_file, short *sample_size_ptr, int *num_samples_ptr) 
 {
-    char chunk_id[] = "    ";  /* chunk id, note initialize as a C-string */
+    char chunk_id[] = "    ";  /* chunk id */
     char data_4[] = "    ";    /* chunk data, 4 bytes */
-    char data_6[] = "      ";  /* chuck data, 6 bytes */
-    int chunk_size = 0;        /* number of bytes remaining in chunk */
+    int chunk_size = 0;        /* number of bytes in chunk */
     short audio_format = 0;    /* audio format type, PCM = 1 */
-    short num_channels = 0;    /* number of audio channels */ 
-    int sample_rate = 0;
+    short num_channels = 0;    
     short bits_per_smp = 0; 
-    int num_samples = 0;
+    int data_bytes = 0;        /* number of bytes of data in all samples*/
 
     /* copy over information from wav_file to new_wav_file */
     fread(chunk_id, 4, 1, wav_file);
@@ -115,10 +116,7 @@ int process_header(FILE *wav_file, FILE *new_wav_file, short *sample_size_ptr, i
     fread(chunk_id, 4, 1, wav_file);
     while(strcmp(chunk_id, "fmt ") != 0) {
         fread(&chunk_size, 4, 1, wav_file);
-        fwrite(&chunk_size, 4, 1, new_wav_file);
-
         fseek(wav_file, chunk_size, SEEK_CUR);
-
         fread(chunk_id, 4, 1, wav_file);
     }
     fwrite(chunk_id, 4, 1, new_wav_file); 
@@ -132,53 +130,46 @@ int process_header(FILE *wav_file, FILE *new_wav_file, short *sample_size_ptr, i
     fread(&num_channels, sizeof(num_channels), 1, wav_file);
     fwrite(&num_channels, sizeof(num_channels), 1, new_wav_file);
 
-    fread(&sample_rate, sizeof(sample_rate), 1, wav_file);
-    fwrite(&sample_rate, sizeof(sample_rate), 1, new_wav_file);
+    fseek(wav_file, 10, SEEK_CUR);                            
 
-    fread(data_6, 6, 1, wav_file);
-    fwrite(data_6, 6, 1, new_wav_file);
+    fread(&bits_per_smp, sizeof(bits_per_smp), 1, wav_file);
+    fwrite(&bits_per_smp, sizeof(bits_per_smp), 1, new_wav_file);
 
-    fread(&bits_per_smp, 1, sizeof(bits_per_smp), wav_file);
-    fwrite(&bits_per_smp, 1, sizeof(bits_per_smp), new_wav_file);
-
-    fread(&chunk_id, 1, 4, wav_file);
+    fread(&chunk_id, 4, 1, wav_file);
     while(strcmp(chunk_id, "data") != 0) {
-        fread(&chunk_size, 4, 1, wav_file);
-        fwrite(&chunk_size, 4, 1, new_wav_file);
-        
+        fread(&chunk_size, 4, 1, wav_file);        
         fseek(wav_file, chunk_size, SEEK_CUR);
-    
         fread(chunk_id, 4, 1, wav_file);
     }
-    fwrite(&chunk_id, 1, 4, new_wav_file);
+    fwrite(&chunk_id, 4, 1, new_wav_file);
 
-    fread(&num_samples, 1, sizeof(num_samples), wav_file);  /* total bytes of data*/
-    fwrite(&num_samples, 1, sizeof(num_samples), new_wav_file);
+    fread(&data_bytes, sizeof(data_bytes), 1, wav_file);
+    fwrite(&data_bytes, sizeof(data_bytes), 1, new_wav_file);
     
     *sample_size_ptr = bits_per_smp / 8;                    /* sample size in bytes */
-    *num_samples_ptr = num_samples / (*sample_size_ptr);    /* total number of audio samples */ 
+    *num_samples_ptr = data_bytes / (*sample_size_ptr);     /* total number of audio samples */ 
 
     return (audio_format == 1);
 }
 
 
 /**
- *  function reads the WAV audio data (last part of the data chunk) to decode message in the least significant bits
+ *  function reads, modifies, and writes samples to and from a .wav file, hiding a message in the LSBs of the new samples
  **/
-void process_data(FILE *wav_file, FILE *new_wav_file, FILE *text_file, short sample_size, int num_samples, int bits) 
+int process_data(FILE *wav_file, FILE *new_wav_file, FILE *text_file, short sample_size, int num_samples, int num_lsb) 
 {
-    unsigned char msg_bits = 0;    /* sequence of bits from the message to be written into new_wav_file samples */
-    unsigned int sample = 0;
-    unsigned int new_sample = 0;   /* sample with modified least significant bits, to be written into new_wav_file */
+    unsigned char msg_bits = 0;        /* sequence of bits from the message to be written into new_wav_file samples */
+    int sample = 0;
+    int new_sample = 0;                /* sample with modified least significant bits, to be written into new_wav_file */
     unsigned int mask = 0;  
     unsigned char smiley[] = ":)";
     unsigned char ch = 0;
-    int msg_samples = 0;          /* number of samples used to hide the message */
-    int num_chars = 0;            /* number of characters in the message */
+    int num_samples_written = 0;
+    int num_chars = 0;           
     int i;
     int j;
 
-    switch (bits) {
+    switch (num_lsb) {
     case 1:
         mask = 0x1; break;    /* .... 0001 */
     case 2:
@@ -189,42 +180,50 @@ void process_data(FILE *wav_file, FILE *new_wav_file, FILE *text_file, short sam
         break;
     }
 
-    /* there would be an issue if the message had to be truncated before the entire charater was processed */
-    /* figure out how to truncate with the smiley face */
     ch = fgetc(text_file);
     /* leave enough samples to encrypt ":)" even if the message has to be truncated. */
-    while ((ch != EOF) && (msg_samples < (num_samples - (16 / bits)))) {
+    while ((ch != EOF) && (num_samples_written < (num_samples - (16 / num_lsb)))) {
         num_chars++;
-        for (i = 8 - bits; i > 0; i -= bits) {
-            fread(&sample, sample_size, 1, wav_file);
-            msg_samples++;
-            if (sample_size == 2) {
-                sample = (unsigned short)sample;                 /* not sure if I have to do this for new_wav_file too */
-            }     
+        for (i = 8 - num_lsb; i > 0; i -= num_lsb) {
             msg_bits = ch & (mask << i);                         /* extract particular bits from message character*/
+
+            fread(&sample, sample_size, 1, wav_file);
+            if (sample_size == 2) {
+                sample = (short)sample;                
+            }     
             new_sample = (~mask & sample) | (msg_bits >> i);     /* turn off LSBs from original sample, place message bits */
+            if (sample_size == 2) {
+                new_sample = (short)new_sample;
+            }
             fwrite(&new_sample, sample_size, 1, new_wav_file);
+            num_samples_written++;
         }
             ch = fgetc(text_file);
     }
-
-    for (i = 0; i < 2; i ++) {
+    for (i = 0; i < 2; i++) {
         ch = smiley[i];
-        for (j = 8 - bits; i > 0; i -= bits) {
+        num_chars++;
+        for (j = 8 - num_lsb; j > 0; j -= num_lsb) {
+            msg_bits = ch & (mask << j);                         /* extract particular bits from message character */
+
             fread(&sample, sample_size, 1, wav_file);
-            msg_samples++;
             if (sample_size == 2) {
-                sample = (unsigned short)sample;                 /* not sure if I have to do this for new_wav_file too */
+                sample = (short)sample;                 
             }     
-            msg_bits = ch & (mask << j);                         /* extract particular bits from message character*/
             new_sample = (~mask & sample) | (msg_bits >> j);     /* turn off LSBs from original sample, place message bits */
+            if (sample_size == 2) {
+                new_sample = (short)new_sample;
+            }
             fwrite(&new_sample, sample_size, 1, new_wav_file);
+            num_samples_written++;
         }
     }
 
     /* copy the rest of the data */
-    while (msg_samples < num_samples) {
+    while (num_samples_written < num_samples) {
         fread(&sample, sample_size, 1, wav_file);
         fwrite(&sample, sample_size, 1, new_wav_file);
+        num_samples_written++;                                     
     }
+    return num_chars;
 }
